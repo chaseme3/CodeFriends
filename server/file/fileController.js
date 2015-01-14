cda94f3344f1f51ed290598bcd5626581aeecb62
Chase Ellsworth <chaseme3@gmail.com>
2015-01-13 17:00:26 -0800

    adds ability to move files 3

--- server/file/fileController.js
+++ server/file/fileController.js
@@ -317,13 +317,6 @@ var fileController = {
       .then(function (oldHash) {
         return backend.submitAsync('documents', oldHash, {
           del: true
-        }, function (err, version, transformedByOps, snapshot) {
-          if (err) {
-            console.log('Error deleting file content: ', err);
-          }
-          console.log('version: ', version);
-          console.log('transformedByOps: ', transformedByOps);
-          console.log('snapshot: ', snapshot);
         });
       })
       .then(function (dbResponse) {
@@ -400,10 +393,10 @@ var fileController = {
         break;
       }
     }
-
     return object;
   }
 
 };
+
 
 module.exports = fileController;
